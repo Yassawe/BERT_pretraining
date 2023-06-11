4a198416239f6597f5f093e32cb820fb1d2d5991
Yassawe <jassaui.k@gmail.com>
2023-06-11 17:31:58 +0900

    init

--- pretrain.py
+++ pretrain.py
@@ -1,17 +1,7 @@
-#!/usr/bin/env python
-# coding=utf-8
-# Copyright 2020 The HuggingFace Team All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
 """
 Pre-training or fine-tuning the library models for masked language modeling (BERT, ALBERT, RoBERTa...) on a text file or a dataset.
 
-Here is the full list of checkpoints on the hub that can be fine-tuned by this script:
+list of supported models:
 https://huggingface.co/models?filter=fill-mask
 """
 
